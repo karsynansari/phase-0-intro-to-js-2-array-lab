9050e0fd0ac0a84e4731c8afa57ecfc3cb2a09d5
karsyn ansari <karsyn.ansari@gmail.com>
2021-12-17 09:21:11 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,30 +1,67 @@
+// const cats = ["Milo", "Otis", "Garfield"];
+
+// function destructivelyAppendCat() {
+//   cats.push("Ralph");
+// }
+// function destructivelyPrependCat() {
+//   cats.unshift("Bob");
+// }
+// function destructivelyRemoveLastCat() {
+//   cats.pop();
+// }
+// function destructivelyRemoveFirstCat() {
+//   cats.shift();
+// }
+// function appendCat() {
+//   const newCat = [...cats, "Broom"];
+//   return newCat;
+// }
+// function prependCat() {
+//   const firstCat = ["Arnold", ...cats];
+//   return firstCat;
+// }
+// function removeLastCat() {
+//   const removeLastCat = cats.slice(0, cats.length - 1);
+//   return removeLastCat;
+// }
+// function removeFirstCat() {
+//   const removeFirstCat = cats.slice(1);
+//   return removeFirstCat;
+// }
+
 const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat() {
   cats.push("Ralph");
 }
+
 function destructivelyPrependCat() {
   cats.unshift("Bob");
 }
+
 function destructivelyRemoveLastCat() {
   cats.pop();
 }
+
 function destructivelyRemoveFirstCat() {
   cats.shift();
 }
+
 function appendCat() {
   const newCat = [...cats, "Broom"];
   return newCat;
 }
 function prependCat() {
-  const firstCat = ["Arnold", ...cats];
-  return firstCat;
+  const addCat = ["Arnold", ...cats];
+  return addCat;
 }
+
 function removeLastCat() {
-  const removeLastCat = cats.slice(0, cats.length - 1);
-  return removeLastCat;
+  const removeACat = cats.slice(0, cats.length - 1);
+  return removeACat;
 }
+
 function removeFirstCat() {
-  const removeFirstCat = cats.slice(1);
-  return removeFirstCat;
+  const removeACat = cats.slice(1);
+  return removeACat;
 }
